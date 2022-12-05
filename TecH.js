bd98b40425ffee94e0706e11a3501d374fb335a9
Ramdeni2 <akshithramdeni2002@gmail.com>
2022-12-05 18:15:33 +0530

    Nxt

---
+++ TecH.js
@@ -0,0 +1,169 @@
+
+/* 
+console.log("Hello Nxt_TecH Learners!....");
+
+console.log("Coding is Easy to learn but you need to Concerate on your goal");
+
+
+*/
+
+
+/*
+     
+     Variables
+
+     variables are placeholders to store the information and data in your App.
+     the variable refers to the value store in it:
+
+*/ 
+
+/* 
+
+
+
+Const - Th  e value cannot be changed 
+
+let- The value can be changed 
+
+var  - The value can be changed  - Not Recommented 
+
+
+*/
+
+
+/*
+const name ='TecH_lovers';
+console.log(name)
+
+const number2 = 1;
+console.log(number2)
+*/
+
+
+
+/*
+                Operators
+                Operators are used to Assign the values,compare the values or perform Airthemetric Operators: 
+                like math Operation +,-,*,/ etc...
+
+                
+*/
+
+
+/*
+   food = 20;
+   tip_amount =1.5;
+   total_amount = (food * tip_amount);
+
+   console.log(total_amount)
+
+*/
+
+/* 
+           User input 
+           Prompt() - is a built-in javascript Functionality that help you to get Input data  from the User through the Browser.
+           alert() - is the Prompt() but it only for the Output:
+
+*/
+
+   /* 
+   Discount Based on Item
+   
+ Discount formula = (discount/100) * (Item_cost)
+
+   */
+
+
+ /*
+
+ const welcome = alert("Welcome to OMSSC ")
+
+  Cycle = prompt("What is your Cycle Cost?: ");
+  console.log(Cycle)
+  discount = prompt('Discount');
+  
+  discount_amount = (discount/100) * Cycle;
+  total_amount = Cycle - discount_amount;
+
+
+console.log( 'discount_Amount: ',discount_amount)
+console.log('Total amount: ',total_amount)
+
+*/
+
+/* 
+
+
+            Data Types:
+            
+            
+            Number = 1,2,3,4,5,6,7,8,9,10....
+            String =  'Hello Coder' , 'I am Programmer' .......
+            Array = []
+            Object = {}
+
+
+*/
+
+
+/* 
+                  Math Method()
+                  floor: The math.floor() method rounds a number down to the nearest integer and returns the result.
+                  ceil : The math.ceil() method rounds a number upto the nearest integer and returns the result.
+*/
+
+                  
+
+/*
+         
+       conditions Statement: 
+                            conditonal are used in statement to compare variable value or datatype 
+                            they are always return a boolean value - True or False.
+
+*/
+
+/*
+
+
+let wheather = prompt("Enter the type of Wheather: ");
+if(wheather == 'rain'){
+    console.log('Grab your umbrella')
+}
+else{
+    console.log('grab your sun glasses')
+}
+                       
+*/
+ 
+// function
+
+/*
+function greeting(name){
+    greet = `hi , ${name} nice to meet you`;
+ console.log(greet)
+}
+greeting('AZMAR');
+
+
+*/
+
+
+
+// Arrow function
+
+
+/*
+const arrow =(a,b) => a+b;
+console.log(arrow(50,100))
+
+const sub = (a,b) => a-b;
+console.log(sub(10,6))
+
+const multi = (a,b) => a*b;
+console.log(multi(10,10))
+
+const div =(a,b) => a/b;
+console.log(div(10,5))
+
+
+*/
